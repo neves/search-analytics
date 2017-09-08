a9e25cfe434657119f1f654e00e938efde2db302
Marcos Neves <marcos.neves@gmail.com>
2017-09-08 15:29:11 -0300

    Implement Search.createOrIncrement

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,13 @@
+require 'digest'
+
 class Search < ApplicationRecord
   self.primary_key = :md5
+
+  def self.createOrIncrement(query)
+    md5 = Digest::MD5.hexdigest(query)
+    Search.create(md5: md5, query: query)
+  rescue ActiveRecord::RecordNotUnique
+    Search.update_counters(md5, hits: 1)
+    Search.find md5
+  end
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'createOrIncrement' do
+    it 'create a Search if query does not already exists' do
+      search = Search.createOrIncrement(rand.to_s)
+      expect(search.persisted?).to be(true)
+    end
+
+    it 'increment a Search if query already exists' do
+      query = 'How do I cancel my subscription'
+      created = Search.createOrIncrement(query)
+      updated = Search.createOrIncrement(query)
+      expect(updated.hits).to be(created.hits + 1)
+    end
+  end
 end
