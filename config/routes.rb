c89abcce65b1e6aee972fd5b26f803762c1dce2f
Marcos Neves <marcos.neves@gmail.com>
2017-09-08 17:01:35 -0300

    Add missing root path

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   resources :articles, only: :index
 
+  root to: 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
