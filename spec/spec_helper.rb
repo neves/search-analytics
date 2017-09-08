8e934fa7d51835858592e3359c209d7eda27f91b
Marcos Neves <marcos.neves@gmail.com>
2017-09-08 15:10:20 -0300

    Truncate database before tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.default_formatter = "doc"
+  config.before(:suite) do
+    DatabaseCleaner.clean_with :truncation
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
