c887839e3497eb286ca8d35235beb3f0a271cae5
Marcos Neves <marcos.neves@gmail.com>
2017-09-08 15:30:52 -0300

    Create scope to order search by highest frequency

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,6 +3,8 @@ require 'digest'
 class Search < ApplicationRecord
   self.primary_key = :md5
 
+  scope :ordered_by_highest_frequency, -> { order(hits: :desc) }
+
   def self.createOrIncrement(query)
     md5 = Digest::MD5.hexdigest(query)
     Search.create(md5: md5, query: query)
